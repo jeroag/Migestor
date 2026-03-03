414b39d41f6105c4e0d69b85d926f60efa3b82f3
jeroag <jeronimoavalosg@gmail.com>
2026-03-03 13:22:04 +0100

    pssd

--- src/db.js
+++ src/db.js
@@ -1,12 +1,9 @@
 const postgres = require('postgres');
 require('dotenv').config();
 
-// Extraemos la URL de la variable de entorno
-const connectionString = process.env.DATABASE_URL;
-
-const sql = postgres(connectionString, {
-  ssl: 'require', // OBLIGATORIO para Supabase en producción
-  connect_timeout: 30, // Da un poco más de margen para conectar
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: 'require',
+  prepare: false // IMPORTANTE: Supabase Pooler requiere esto en 'false' a veces
 });
 
 module.exports = sql;
