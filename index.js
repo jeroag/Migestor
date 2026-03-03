94f08fc346ed45b13f36402b3fc7f42e2ae28d1b
jeroag <jeronimoavalosg@gmail.com>
2026-03-03 12:55:55 +0100

    Corrigiendo rutas

--- index.js
+++ index.js
@@ -101,4 +101,6 @@ app.get('/resumen', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor listo en el puerto ${PORT}`);
+});
